3884359a266ae3e7ea4cca25c75e24f10f19c81f
jacekroszkowiak <jacekroszkowiakdev@gmail.com>
2025-03-27 00:17:37 +0100

    feat: broadcast messages and handle basic WebSocket errors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,7 +2,7 @@ import express from "express";
 import http from "http";
 import WebSocket, { WebSocketServer } from "ws";
 import { v4 as uuidv4 } from "uuid";
-import { User } from "./types";
+import { User, Message } from "./types";
 
 const app = express();
 const server = http.createServer(app);
@@ -10,11 +10,22 @@ const wss = new WebSocketServer({ server });
 const PORT = 3001;
 
 const users: Map<string, User> = new Map();
+const messages: Message[] = [];
 
 function generateColorCode(): string {
     return `#${Math.floor(Math.random() * 0xffffff)
         .toString(16)
         .padStart(6, "0")}`;
+}
+
+function broadcastMessage(message: Message) {
+    const messageString = JSON.stringify(message);
+
+    wss.clients.forEach((client) => {
+        if (client.readyState === WebSocket.OPEN) {
+            client.send(messageString);
+        }
+    });
 }
 
 function broadcastParticipants() {
@@ -46,19 +57,30 @@ wss.on("connection", (socket: WebSocket) => {
         name: `User_${userId.slice(0, 7)}`,
         color: generateColorCode(),
     });
-    console.log("Users:", users);
     console.log(`${userId} joined with name ${users.get(userId)?.name}`);
     broadcastParticipants();
 
     socket.on("message", (message) => {
-        console.log("Received: " + message);
-        socket.send("Echo: " + message);
+        try {
+            const parsedMessage = JSON.parse(message.toString());
+            const user = users.get(parsedMessage.userId);
+            parsedMessage.id = uuidv4();
+            parsedMessage.userId = userId;
+            parsedMessage.userName = `${users.get(userId)?.name}`;
+            parsedMessage.content = "text";
+            parsedMessage.createdAt = new Date();
+            messages.push(parsedMessage);
+            console.log("Messages array:", messages);
+            broadcastMessage(parsedMessage);
 
-        wss.clients.forEach((client) => {
-            if (client.readyState === WebSocket.OPEN) {
-                client.send(message);
-            }
-        });
+            wss.clients.forEach((client) => {
+                if (client.readyState === WebSocket.OPEN) {
+                    client.send(message);
+                }
+            });
+        } catch (error) {
+            console.log("Error parsing message:", error);
+        }
     });
 
     socket.on("close", () => {
@@ -72,6 +94,10 @@ wss.on("connection", (socket: WebSocket) => {
             console.log(`User ${disconnectedUser?.id} disconnected`);
             broadcastParticipants();
         }
+    });
+
+    socket.on("error", (error) => {
+        console.log("Error:", error);
     });
 });
 
